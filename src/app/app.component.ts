472806e057b232b3f18849f037c9875e1b252ac3
Amadeja <obsteter.amadeja@gmail.com>
2024-11-29 05:56:24 +0100

    Add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomepageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomepageComponent},
+    {path: 'login', component: LoginComponent},
+];
 
 // https://angular.dev/guide/routing/router-tutorial
